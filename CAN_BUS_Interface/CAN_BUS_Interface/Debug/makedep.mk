4e77a754f0f21d9c2a8fa039bf47445f9f2d905b
ZeroX96 <msammamsa10@gmail.com>
2021-07-09 07:49:01 +0200

    ZeroX96

---
+++ CAN_BUS_Interface/CAN_BUS_Interface/Debug/makedep.mk
@@ -0,0 +1,12 @@
+################################################################################
+# Automatically-generated file. Do not edit or delete the file
+################################################################################
+
+CAN_MCP2515\ecu_CAN.c
+
+EXT_INTERRUPTS\external_interrupts.c
+
+main.c
+
+SPI\hal_SPI.c
+
